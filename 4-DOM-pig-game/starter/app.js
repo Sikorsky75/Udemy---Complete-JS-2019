ef7c25ed57205d6a2efdd0ba0d926caf8cb6b7ed
Sikorsky75 <sikorsky75@gmail.com>
2019-05-07 15:24:42 +0200

    commit

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,14 +15,32 @@ scores = [0,0];
 rounScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+    
+    // update round score if rolled number was not a 1
+
+});
