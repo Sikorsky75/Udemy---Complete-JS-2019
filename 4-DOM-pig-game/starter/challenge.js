193ed687e4fa1a0f15902fc2eb4346a5a165eaa7
123456sikorsky <sikorsky75@gmail.com>
2019-05-19 22:11:03 +0200

    19.05.2019 coding challenge 2

--- 4-DOM-pig-game/starter/challenge.js
+++ 4-DOM-pig-game/starter/challenge.js
@@ -9,7 +9,11 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, lastDice, winScore;
+
+
+// winScore = 100;
+//winScore = document.getElementById('winScoreInput').value;
 
 init();
 
@@ -19,6 +23,7 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
     document.querySelector('.dice').style.display = 'none';
+    
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -35,22 +40,33 @@ function init() {
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     
+    winScore = document.getElementById('winScoreInput').value;
+
+
     if(gamePlaying) {
         // random number
+        
         dice = Math.floor(Math.random() * 6) + 1;
+              
 
         //display result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
-    
-        // update round score if rolled number was not a 1
-        if (dice !== 1) {
+        
+        if (dice === 6 && lastDice === 6) {
+           scores[activePlayer] = 0;
+           document.querySelector('#score-' + activePlayer).textContent = '0';
+           nextPlayer();
+
+        } else if (dice !== 1) {
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             nextPlayer();
-        }  
+        } 
+
+        lastDice = dice;
     }    
 });
 
@@ -66,7 +82,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if player won game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= winScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
